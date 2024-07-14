ad7f7f6d2c1bee65d498cec27a145f2546ccfb1a
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2024-07-14 12:59:25 +0100

    added skills

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Masthead from "@/components/masthead";
 import AboutUs from "@/components/aboutus";
+import Skills from "@/components/skills";
 
 export default function Home() {
   return (
     <>
       <Masthead />
       <AboutUs />
+      <Skills />
     </>
   );
 }
---
+++ components/skills.tsx
@@ -0,0 +1,19 @@
+const Skills = () => {
+  return (
+    <div className="bg-black text-white">
+      <div className="min-h-screen max-w-5xl mx-auto px-10 lg:px-20 py-24 md:py-28 lg:py-36 flex flex-col justify-center items-center text-4xl md:text-6xl tracking-tight font-semibold">
+        <div className="leading-[1.15]">
+          <div>Votre satisfaction est notre priorité.</div>
+          <span>
+          Notre studio de design graphique & digital, est à votre écoute pour mettre en place votre stratégie digitale en commençant par la <strong>conception de votre Marque:</strong> logo, carte visite, flyer, brochures…
+          </span><br />
+          <span>
+            <strong>Jusqu&apos;au site web:</strong> conception, développement, mise en ligne et référencement.
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
