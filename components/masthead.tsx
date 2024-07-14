ad03131b789a9b7b7b1725dc02325fc4fdf98d0a
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2024-07-14 12:25:58 +0100

    added scroll and logo animations

--- components/masthead.tsx
+++ components/masthead.tsx
@@ -1,9 +1,33 @@
+"use client";
+
 import { useRef, useContext, useState, useCallback } from "react";
 import Image from "next/image";
+import { ScrollContext } from "@/utils/scroll-observer";
 
 const Masthead = () => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const refContainer = useRef<HTMLDivElement>(null);
+  const { scrollY } = useContext(ScrollContext);
+
+  let progress = 0;
+
+  const { current: elContainer } = refContainer;
+  if (elContainer) {
+    progress = Math.min(1, scrollY / elContainer.clientHeight);
+  }
+
+  const handleImageLoaded = useCallback(() => {
+    setImageLoaded(true);
+  }, []);
+
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center ">
+    <div
+      ref={refContainer}
+      className="min-h-screen flex flex-col items-center justify-center sticky top-0 -z-10"
+      style={{
+        transform: `translateY(-${progress * 20}vh)`,
+      }}
+    >
       <video
         autoPlay
         loop
@@ -13,7 +37,9 @@ const Masthead = () => {
       >
         <source src="/assets/bubbles-loop.webm" type="video/webm; codecs=vp9" />
       </video>
-      <div className={`flex-grow-0 z-10 pt-6 transition-opacity duration-1000`}>
+      <div className={`flex-grow-0 z-10 pt-6 transition-opacity duration-1000 ${
+        imageLoaded ? 'opacity-100' : 'opacity-0'
+      }`}>
         <Image
           src="/assets/Logo.png"
           alt="logo"
@@ -27,12 +53,17 @@ const Masthead = () => {
           <span>Design,</span> <span>done right.</span>
         </h2>
       </div>
-      <div className="flex-grow-0 z-10 pb-5 md:pb-2 transition-all duration-1000">
+      <div
+        className={`flex-grow-0 z-10 pb-5 md:pb-2 transition-all duration-1000 ${
+          imageLoaded ? "opacity-100" : "opacity-0 -translate-y-10"
+        }`}
+      >
         <Image
           src="/assets/down-arrow.png"
           width={188 / 3}
           height={105 / 3}
           alt="scroll down"
+          onLoad={handleImageLoaded}
         />
       </div>
     </div>
