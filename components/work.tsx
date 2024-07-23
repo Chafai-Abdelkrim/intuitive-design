c89c0be30685aecdf81361ecc37019c7893199b4
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2024-07-23 13:31:43 +0100

    added workLeft and workRight

--- components/work.tsx
+++ components/work.tsx
@@ -1,14 +1,63 @@
 import Link from "next/link";
 
-export const WorkContainer = ({ children }: { children: React.ReactNode }) => (
+export const WorkContainer = ({ children }: { children?: React.ReactNode }) => (
   <div className="grid grid-cols-1 lg:grid-cols-2 w-full min-h-screen">
     {children}
   </div>
 );
 
-export const WorkBackground = ({ children }: { children: React.ReactNode }) => (
-    <div className="grid grid-cols-1 lg:grid-cols-2 w-full min-h-screen top-0 sticky">
-        <div className="bg-black h-[30vh] lg:h-auto"></div>
-        <div className="bg-black h-[70vh] lg:min-h-screen"></div>
+export const WorkBackground = ({
+  children,
+}: {
+  children?: React.ReactNode;
+}) => (
+  <div className="grid grid-cols-1 lg:grid-cols-2 w-full min-h-screen top-0 sticky">
+    <div className="bg-black h-[30vh] lg:h-auto"></div>
+    <div className="bg-black h-[70vh] lg:min-h-screen"></div>
+  </div>
+);
+
+export const WorkLeft = ({
+  children,
+  progress,
+}: {
+  children?: React.ReactNode;
+  progress: number;
+}) => {
+  let translateY = Math.max(0, 50 - progress * 3 * 50);
+  if (progress > 0.85) translateY = Math.max(-50, -(progress - 0.85) * 2 * 50);
+
+  return (
+    <div
+      className="flex flex-col items-center justify-center text-3xl h-[30vh] lg:h-auto"
+      style={{
+        transform: `translateY(${translateY}px)`,
+      }}
+    >
+      <div className="leading-10">{children}</div>
     </div>
-)
+  );
+};
+
+export const WorkRight = ({
+  children,
+  progress,
+}: {
+  children?: React.ReactNode;
+  progress: number;
+}) => {
+  let translateY = Math.max(-50, -(progress - 0.5) * 50);
+
+  return (
+    <div
+      className="flex flex-1 lg:items-center justify-center h-screen"
+      style={{
+        transform: `translateY(${translateY}px)`,
+      }}
+    >
+      <div className="w-full max-w-md pt-10 lg:pt-0 px-10 md:px-0">
+        {children}
+      </div>
+    </div>
+  );
+};
--- components/works.tsx
+++ components/works.tsx
@@ -1,15 +1,29 @@
-import { TileBackground, TileContent, TileWrapper } from "./tile";
+import { Tile, TileBackground, TileContent, TileWrapper } from "./tile";
+import { WorkBackground, WorkContainer, WorkLeft } from "./work";
 
 const Works = () => {
-    return (
-       <TileWrapper numOfPages={3}>
-            <TileBackground></TileBackground>
-            <TileContent>Foo</TileContent>
-            <TileContent>bar</TileContent>
-            <TileContent>baz</TileContent>
-       </TileWrapper>
-       
-    )
-}
+  return (
+    <TileWrapper numOfPages={3}>
+      <TileBackground>
+        <WorkBackground />
+      </TileBackground>
+      <TileContent>
+        <Tile
+          page={0}
+          renderContent={({ progress }) => (
+            <WorkContainer>
+                <WorkLeft progress={progress}>
+                    <div>We Built</div>
+                </WorkLeft>
+              <span>{progress}</span>
+            </WorkContainer>
+          )}
+        ></Tile>
+      </TileContent>
+      <TileContent>bar</TileContent>
+      <TileContent>baz</TileContent>
+    </TileWrapper>
+  );
+};
 
 export default Works;
