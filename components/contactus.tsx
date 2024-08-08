125bdfbefad67e3ad34c23dfa7683f7fb063db69
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2024-08-08 11:27:49 +0100

    fixed blanc gap

--- components/contactus.tsx
+++ components/contactus.tsx
@@ -2,8 +2,11 @@ import Image from "next/image";
 
 const ContactUs = () => {
   return (
-    <div className="bg-black text-white flex flex-col justify-center pt-10 min-h-screen">
-        
+    <div className="bg-black text-white flex flex-col justify-center items-center pt-10 min-h-screen">
+        <div className="pb-10">
+            <Image src="/assets/Logo.png" width={65} height={65} alt="logo" />
+        </div>
+        <h2 className="text-4xl font-bold">Contact Us</h2>
     </div>
   );
 };
