dba54a43dde319f7c5941a15d709644a4261af0a
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2024-08-09 13:13:40 +0100

    updated

--- components/trustedby.tsx
+++ components/trustedby.tsx
@@ -3,19 +3,25 @@ import Testimonials from "./testimonials";
 import s from "../app/trustedby.module.css";
 
 const TrustedBy = () => (
-  <section className={`bg-white min-h-screen flex flex-col justify-center gap-16 md:gap-32 ${s.bg}`}>
+  <section
+    className={`bg-white min-h-screen flex flex-col justify-center gap-16 md:gap-32 ${s.bg}`}
+  >
     <div className="flex-1"></div>
     <div className="flex flex-col justify-center items-center">
-        <h3 className="text-xl mb-10 font-bold text-center">
-            <span className="whitespace-nowrap">Trusted By</span>{' '}
-            <span className="whitespace-nowrap">some of the apps you use daily.</span>
-        </h3>
-        <ClientLogos />
+      <h3 className="text-xl mb-10 px-4 font-bold text-center">
+        <span className="whitespace-nowrap">Approuvé par</span>{" "}
+        <span className="whitespace-normal">
+          certaines des entreprises que vous connaissez.
+        </span>
+      </h3>
+      <ClientLogos />
     </div>
     <div className="flex flex-col justify-center items-center">
-        <div className="container mx-auto lg:max-w-[70%] lg:px-10">
-            <h3 className="text-3xl lg:text-4xl tracking-tight text-center px-10 !leading-[3.5rem]">We belive in good comunication and a full transparent procces.</h3>
-        </div>
+      <div className="container mx-auto lg:max-w-[70%] lg:px-10">
+        <h3 className="text-3xl lg:text-4xl tracking-tight text-center px-10 !leading-[3.5rem]">
+          Nous croyons en une bonne communication et un processus totalement transparent.
+        </h3>
+      </div>
     </div>
     <Testimonials />
     <div className="flex-1 bg-black"></div>
